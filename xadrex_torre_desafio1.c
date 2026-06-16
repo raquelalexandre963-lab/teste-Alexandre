26dea255089f03ed817ec326d194957be5b8d3cd
raquelalexandre963-lab <raquelalexandre963@gmail.com>
2026-06-16 01:08:14 +0000

    primeiro desafio novato feito

---
+++ xadrex_torre_desafio1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    //mover a torre 5 casa para a direita
+    for(int i = 0; i < 5; i++) {
+        printf("direita\n"); //imprimir a direcao do movimento
+    }
+    return 0;
+}
