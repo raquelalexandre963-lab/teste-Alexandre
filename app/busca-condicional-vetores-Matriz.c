24831f3751971ec6bbc2b7fc8ea21916c43672c4
Alexandre <teu@email.com>
2026-06-22 21:50:01 -0300

    salvo com sucesso

--- app/busca-condicional-vetores-Matriz.c
+++ app/busca-condicional-vetores-Matriz.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
-
+ 
 int main() {
-    int matriz[3][3] = {{1,2,3}, {4,5,6}, {7,8,9}};
-    int targed = 5;
+    int matriz[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int target = 5;
     int found = 0;
-    //Busca condicional do elemento alvo
-    for (int i = 1, i < 3; i++) { 
-        for (int j = 0, j < 3; j++) {
-            if (matriz[i][j] == targed) {
-                printf("elemento %d encontrado na posicao (%d, %d)\n", targed, i, j);
-                found = 1
+ 
+    // Busca condicional do elemento alvo
+    for (int i = 0; i < 3; i++) {      // Loop externo para as linhas
+        for (int j = 0; j < 3; j++) {  // Loop interno para as colunas
+            if (matriz[i][j] == target) {
+                printf("Elemento %d encontrado na posição (%d, %d)\n", target, i, j);
+                found = 1;
                 break;
             }
         }
         if (found) break;
-        
     }
-         
-          if (!found) {
-            printf("elemento %d nao encontrado na matriz\n", targed);
-           }
-    
+ 
+    if (!found) {
+        printf("Elemento %d não encontrado na matriz\n", target);
+    }
+ 
     return 0;
 }
